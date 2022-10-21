01e73611fe715c7f350eb600ae1a62c0356db9ee
Aleksejs Buguks <106954466+aib84@users.noreply.github.com>
2022-10-21 15:23:14 +0200

    Delete Новая папка directory

--- Новая папка/js/script-dist.js
+++
@@ -1,1 +0,0 @@
-jQuery((function(s){s(".testimonials__slider").slick({horizontal:!0,arrows:!0,slidesToShow:1,slidesToScroll:1,speed:300,easing:"easeInOutBack",responsive:[{breakpoint:479,settings:{arrows:!1,dots:!0}}]})}));
